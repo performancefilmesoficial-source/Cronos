09bbd9fc5c18018615a277876bfab48b4ae342db
performancefilmesoficial-source <performancefilmesoficial@gmail.com>
2026-03-09 12:50:08 -0300

    add Dockerfile and update next config for standalone output

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   devIndicators: {
     appIsrStatus: false, // Pode ser que em algumas subversões aceite, mas Next 15 com Turbopack usa `position`
     buildActivityPosition: 'bottom-right',
